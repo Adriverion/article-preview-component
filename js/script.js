af65b360c9465f672d0995f9ea8c57e4d4abeea0
Adriverion <dantejose7@gmail.com>
2024-10-09 20:17:03 -0400

    added desktop view, restructured styles and simplified the script

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@ const profileView = document.getElementById('profileView');
 const socialView = document.getElementById('socialView');
 
 shareButton.addEventListener('click', event => {
-    let hidden = 'article__contact-box--hidden';
-
-    if (profileView.classList.contains(hidden)) {
-        socialView.classList.add(hidden);
-        profileView.classList.remove(hidden);
-    } else {
-        socialView.classList.remove(hidden);
-        profileView.classList.add(hidden);
-    }
+    socialView.classList.toggle('article__contact-box--hidden');
     shareButton.classList.toggle('article__button--dark');
 });
